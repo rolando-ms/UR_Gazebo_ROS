fb24d551f55fd5b3d361567dc056729e6ea92741
rolando-ms <rolando.moralessuarez@gmail.com>
2021-08-18 12:04:03 +0200

    Function network control implemented. Creates MQTT connection to send gazebo data and receive inference result from Neural Network model.

--- move_ur.py
+++ move_ur.py
@@ -3,35 +3,36 @@
 # Main script
 # Lets the user run the keyboard control or network control (not yet implemented) functions
 
-import math
 from Keyboard_Controller import KeyboardMoveJointsGazebo
+import math
+import numpy as np
+from paho.mqtt import client as mqtt_client
+from Publishers import move_to_initial_pose
+from Publishers import MQTT_pub_sub
 from Publishers import ROS_pub_sub
 import pygame
 from pygameKeyPolling import PygWindow
 import Queue
-from threadClass import NewThread
-from trajectory_msgs.msg import JointTrajectoryPoint
+import random
 import rospy
+from threadClass import NewThread
+import time
+from trajectory_msgs.msg import JointTrajectoryPoint
 
 
-# Moves robot to given pose until threshold is reached
-def move_to_initial_pose(pub_subs, pose, threshold = 0.01):
-    dist = 1e6
-    steps = 0
-    while(dist > threshold):
-        pub_subs.publish(pose)
-        current_pos = pub_subs.current_pos #rospy.wait_for_message("joint_states", JointState).position
-        dist = euclidean_distance(current_pos, pose)
-        # print("current pos = {0}".format(current_pos))
-        print("distance = {0}".format(dist))
-        steps += 1
+def arr_to_str(array):
+    string = ''
+    for item in array:
+        string += str(item)
+        string += ' '
+    return string
 
 
-def euclidean_distance(current_pos, desired_pos):
-    sqr_sum = 0
-    for i in range(len(current_pos)):
-        sqr_sum += (desired_pos[i] - current_pos[i])**2
-    return math.sqrt(sqr_sum)
+def str_to_arr(string):
+    lst = []
+    for item in string.split():
+        lst.append(float(item))
+    return lst
 
 
 def options_menu():
@@ -75,7 +76,50 @@ def Keyboard_Control():
 
 
 def Network_Control():
-    print("Not implemented.")
+    joint_names = ['shoulder_pan_joint', 'shoulder_lift_joint',
+                   'elbow_joint', 'wrist_1_joint', 'wrist_2_joint',
+                   'wrist_3_joint']
+
+    # Instantiating ROS publisher-subscriber object
+    ros_pub_sub = ROS_pub_sub(node_name='move_joints',
+                          pub_topic='/arm_controller/command',
+                          sub_topic='/joint_states',
+                          joint_names=joint_names,
+                          rate=60,
+                          speed=1)
+
+    # Moving to initial position
+    desired_pose = [0., -math.pi / 2, 0., -math.pi / 2, 0., 0.]  # [0, 0, 0, 0, 0, 0]
+    move_to_initial_pose(pub_subs=ros_pub_sub, pose=desired_pose)
+
+    # Instantiating MQTT publisher-subscriber object
+    mqtt_pub_sub = MQTT_pub_sub()
+
+    last_msg = []
+    goal = [0., 0., 0.75]
+
+    while True:
+        time.sleep(0.5)
+
+        # Publishing
+        msg = np.ones(15)
+        msg = ros_pub_sub.current_pos
+        msg.extend(ros_pub_sub.current_vel)
+        msg.extend(goal)
+        msg = arr_to_str(msg)
+        mqtt_pub_sub.publish(msg)
+
+        # Subscribing
+        mqtt_pub_sub.subscribe()
+        print("Subscriber message = {0}".format(str_to_arr(mqtt_pub_sub.sub_msg)))
+        sub_msg_list = str_to_arr(mqtt_pub_sub.sub_msg)
+        # if sub_msg_list == last_msg:
+        #     mqtt_pub_sub.sub_msg = ' '
+        # last_msg = sub_msg_list
+
+        if len(sub_msg_list) > 0:
+            ros_pub_sub.publish(goal=sub_msg_list)
+
 
 def main():
     exit = False
