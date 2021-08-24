fea62e86c434265e5675f465f8b81a0c7f963a6b
rolando-ms <rolando.moralessuarez@gmail.com>
2021-08-24 12:33:03 +0200

    Tf listener added. It is used to build target vector

--- move_ur.py
+++ move_ur.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python
 
 # Main script
-# Lets the user run the keyboard control or network control (not yet implemented) functions
+# Lets the user run the keyboard control or network control functions
 
 from Keyboard_Controller import KeyboardMoveJointsGazebo
 import math
 import numpy as np
 from paho.mqtt import client as mqtt_client
-from Publishers import move_to_initial_pose
-from Publishers import MQTT_pub_sub
-from Publishers import ROS_pub_sub
+from Publishers_Subscribers import move_to_initial_pose
+from Publishers_Subscribers import MQTT_pub_sub
+from Publishers_Subscribers import ROS_pub_sub
+from Publishers_Subscribers import ROS_tf_listener
 import pygame
 from pygameKeyPolling import PygWindow
 import Queue
 import random
 import rospy
+import tf
 from threadClass import NewThread
 import time
-from trajectory_msgs.msg import JointTrajectoryPoint
 
 
 def arr_to_str(array):
@@ -98,27 +99,65 @@ def Network_Control():
     last_msg = []
     goal = [0., 0., 0.75]
 
-    while True:
-        time.sleep(0.5)
+    tf_listener = ROS_tf_listener(reference_frame='/world', target_frame='/wrist_3_link')
+    # point_in_reference = [0.,0.,0.,1.] # x,y,z,scaling
+    base_offset = np.array([0.5, 0., 0.5])
 
-        # Publishing
+    # Point to transform with its reference frame (Taking the origin of the reference frame)
+    tf_listener.BuildPointStamped(point=[0.,0.,0.])
+
+
+    while True:
+        time.sleep(0.25)
+
+        trans = []
+        rot = []
+        trans, rot = tf_listener.LookUpTransform()
+        point_in_new_frame = tf_listener.TransformPoint()
+        print("Trans = {} , rot = {}".format(trans, rot))
+        print("Point in frame {} = [{},{},{}] , Point in frame {} = [{},{},{}]".format(
+            tf_listener.target_frame,
+            tf_listener.point_stamped.point.x,
+            tf_listener.point_stamped.point.y,
+            tf_listener.point_stamped.point.z,
+            tf_listener.reference_frame,
+            point_in_new_frame.point.x + base_offset[0],
+            point_in_new_frame.point.y + base_offset[1],
+            point_in_new_frame.point.z * -1. + base_offset[2]
+        ))
+
+        # Computing target vector
+        world_point = np.array([point_in_new_frame.point.x + base_offset[0],
+                                point_in_new_frame.point.y + base_offset[1],
+                                -1. * point_in_new_frame.point.z + base_offset[2]])
+        world_target = np.array(goal)
+        to_target = world_target - world_point
+
+        # Joint limits
+        dof_lower_limits = np.array([-np.pi]*6)
+        dof_upper_limits = np.array([np.pi]*6)
+
+        # Sending observations for inference
         msg = np.ones(15)
-        msg = ros_pub_sub.current_pos
-        msg.extend(ros_pub_sub.current_vel)
-        msg.extend(goal)
+        dof_pos = ros_pub_sub.current_pos
+        dof_pos_scaled = (2.0 * (dof_pos - dof_lower_limits)
+                          / (dof_upper_limits - dof_lower_limits) - 1.0)
+        msg = dof_pos_scaled.tolist()
+        msg.extend(np.array(ros_pub_sub.current_vel) * 0.01)
+        msg.extend(to_target)
         msg = arr_to_str(msg)
         mqtt_pub_sub.publish(msg)
 
-        # Subscribing
+        # Receiving inference result
         mqtt_pub_sub.subscribe()
         print("Subscriber message = {0}".format(str_to_arr(mqtt_pub_sub.sub_msg)))
         sub_msg_list = str_to_arr(mqtt_pub_sub.sub_msg)
-        # if sub_msg_list == last_msg:
-        #     mqtt_pub_sub.sub_msg = ' '
-        # last_msg = sub_msg_list
 
+        # Updating simulation
+        sub_msg_np = np.array(sub_msg_list)
+        sub_msg_np*=np.pi
         if len(sub_msg_list) > 0:
-            ros_pub_sub.publish(goal=sub_msg_list)
+            ros_pub_sub.publish(goal=sub_msg_np)
 
 
 def main():
