785a7bade63e0af8803971bbbde7799712e931cc
rolando-ms <rolando.moralessuarez@gmail.com>
2021-09-07 09:55:27 +0200

    Pipeline splitted. Just publishing observations to MQTT

--- move_ur.py
+++ move_ur.py
@@ -3,13 +3,21 @@
 # Main script
 # Lets the user run the keyboard control or network control functions
 
+from Helpers import arr_to_str
+from Helpers import move_to_initial_pose
+from Helpers import move_to_initial_pose2
+from Helpers import str_to_arr
 from Keyboard_Controller import KeyboardMoveJointsGazebo
 import math
+from multipledispatch import dispatch
 import numpy as np
-from paho.mqtt import client as mqtt_client
-from Publishers_Subscribers import move_to_initial_pose
+# from paho.mqtt import client as mqtt_client
+from Publishers_Subscribers import MQTT_pub
 from Publishers_Subscribers import MQTT_pub_sub
+from Publishers_Subscribers import MQTT_sub
+from Publishers_Subscribers import ROS_pub
 from Publishers_Subscribers import ROS_pub_sub
+from Publishers_Subscribers import ROS_sub
 from Publishers_Subscribers import ROS_tf_listener
 import pygame
 from pygameKeyPolling import PygWindow
@@ -19,21 +27,6 @@ import rospy
 import tf
 from threadClass import NewThread
 import time
-
-
-def arr_to_str(array):
-    string = ''
-    for item in array:
-        string += str(item)
-        string += ' '
-    return string
-
-
-def str_to_arr(string):
-    lst = []
-    for item in string.split():
-        lst.append(float(item))
-    return lst
 
 
 def options_menu():
@@ -65,7 +58,7 @@ def Keyboard_Control():
                           pub_topic='/arm_controller/command',
                           sub_topic='/joint_states',
                           joint_names=joint_names,
-                          rate = 60,
+                          rate=60,
                           speed=1)
 
     # Moving to initial position
@@ -81,34 +74,52 @@ def Network_Control():
                    'elbow_joint', 'wrist_1_joint', 'wrist_2_joint',
                    'wrist_3_joint']
 
+    rate = 60
+
     # Instantiating ROS publisher-subscriber object
-    ros_pub_sub = ROS_pub_sub(node_name='move_joints',
-                          pub_topic='/arm_controller/command',
+    ros_pub = ROS_pub(node_name='move_joints_sub',
+                      pub_topic='/arm_controller/command',
+                      joint_names=joint_names,
+                      rate=rate)
+
+    # Instantiating ROS publisher-subscriber object
+    ros_sub = ROS_sub(node_name='move_joints_sub',
                           sub_topic='/joint_states',
                           joint_names=joint_names,
-                          rate=60,
-                          speed=1)
+                          rate=rate)
 
     # Moving to initial position
-    desired_pose = [0., -math.pi / 2, 0., -math.pi / 2, 0., 0.]  # [0, 0, 0, 0, 0, 0]
-    move_to_initial_pose(pub_subs=ros_pub_sub, pose=desired_pose)
+    desired_pose = [0., -np.pi / 2, 0., -np.pi / 2, 0., 0.]  # [0, 0, 0, 0, 0, 0]
+    # desired_pose = [0.] * 6
+    # move_to_initial_pose(pub_subs=ros_pub_sub, pose=desired_pose)
+    move_to_initial_pose2(pub=ros_pub, sub=ros_sub, pose=desired_pose)
 
     # Instantiating MQTT publisher-subscriber object
-    mqtt_pub_sub = MQTT_pub_sub()
+    # mqtt_pub_sub = MQTT_pub_sub()
+    mqtt_pub = MQTT_pub()
 
     last_msg = []
-    goal = [0., 0., 0.75]
+    goal = [0., 0., 1.75]
+    # goal = [0.5, -0.16, 1.93]
 
     tf_listener = ROS_tf_listener(reference_frame='/world', target_frame='/wrist_3_link')
     # point_in_reference = [0.,0.,0.,1.] # x,y,z,scaling
-    base_offset = np.array([0.5, 0., 0.5])
+    base_offset = np.array([0.5, 0., 1.5])
 
     # Point to transform with its reference frame (Taking the origin of the reference frame)
     tf_listener.BuildPointStamped(point=[0.,0.,0.])
 
+    # Joint position limits
+    dof_lower_limits = np.array([-np.pi] * 6)
+    dof_upper_limits = np.array([np.pi] * 6)
+
+    # Joint velocity limits
+    dof_vel_lower_limits = np.array([-2.16, -2.16, -3.2, -3.2, -3.2, -3.2])
+    dof_vel_upper_limits = -1 * dof_vel_lower_limits
+
 
     while True:
-        time.sleep(0.25)
+        time.sleep(1./rate)
 
         trans = []
         rot = []
@@ -129,35 +140,36 @@ def Network_Control():
         # Computing target vector
         world_point = np.array([point_in_new_frame.point.x + base_offset[0],
                                 point_in_new_frame.point.y + base_offset[1],
-                                -1. * point_in_new_frame.point.z + base_offset[2]])
+                                -1. * (point_in_new_frame.point.z - base_offset[2])])
         world_target = np.array(goal)
         to_target = world_target - world_point
 
-        # Joint limits
-        dof_lower_limits = np.array([-np.pi]*6)
-        dof_upper_limits = np.array([np.pi]*6)
-
         # Sending observations for inference
         msg = np.ones(15)
-        dof_pos = ros_pub_sub.current_pos
+        dof_pos = ros_sub.current_pos
+        dof_pos = np.array(dof_pos)
+        dof_pos = dof_pos.clip(dof_lower_limits, dof_upper_limits)
         dof_pos_scaled = (2.0 * (dof_pos - dof_lower_limits)
                           / (dof_upper_limits - dof_lower_limits) - 1.0)
         msg = dof_pos_scaled.tolist()
-        msg.extend(np.array(ros_pub_sub.current_vel) * 0.01)
+        current_vel = np.array(ros_sub.current_vel)
+        current_vel = current_vel.clip(dof_vel_lower_limits, dof_vel_upper_limits)
+        msg.extend(current_vel * 0.1)
         msg.extend(to_target)
         msg = arr_to_str(msg)
-        mqtt_pub_sub.publish(msg)
+        mqtt_pub.publish(msg)
 
-        # Receiving inference result
-        mqtt_pub_sub.subscribe()
-        print("Subscriber message = {0}".format(str_to_arr(mqtt_pub_sub.sub_msg)))
-        sub_msg_list = str_to_arr(mqtt_pub_sub.sub_msg)
-
-        # Updating simulation
-        sub_msg_np = np.array(sub_msg_list)
-        sub_msg_np*=np.pi
-        if len(sub_msg_list) > 0:
-            ros_pub_sub.publish(goal=sub_msg_np)
+        # # Receiving inference result
+        # mqtt_pub_sub.subscribe()
+        # print("Subscriber message = {0}".format(str_to_arr(mqtt_pub_sub.sub_msg)))
+        # sub_msg_list = str_to_arr(mqtt_pub_sub.sub_msg)
+        #
+        # # Updating simulation
+        # sub_msg_np = np.array(sub_msg_list)
+        # # sub_msg_np*=np.pi
+        # if len(sub_msg_list) > 0:
+        #     sub_msg_np = ((sub_msg_np + 1) * (dof_upper_limits - dof_lower_limits)) / 2 + dof_lower_limits
+        #     ros_pub_sub.publish(goal=sub_msg_np)
 
 
 def main():
